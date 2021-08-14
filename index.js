e80ae42148e604a3bcfd59489bcb11f05a418416
SGokulkumar05 <gokulsenapthi05@gmail.com>
2021-08-14 19:15:47 +0530

    commit 2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const mongoose=require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
+const path  = require('path');
 app=express();
 
 app.use(cors());
